c21c18837481309bed32c1192d543c1383612073
Pham Thanh Khang <ptkkhang987@gmail.com>
2025-10-24 11:08:06 +0700

    Commit cicd

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -107,8 +107,17 @@ class ProductController {
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Server error" });
-    }
+    } 
   }
+  async getProductById(req, res, next) {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  return res.status(200).json(product);
+}
+
 }
 
 module.exports = ProductController;
